5ec6aa2902c6930fcffa8f143fa73ad888aebd7f
Chalapathi Nandiwada <chalaapthirao471@outlook.com>
2020-09-18 21:27:11 +0530

    first commit

---
+++ src/main/java/com/example/AzureDemoWebApp/AzureDemoWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AzureDemoWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDemoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDemoWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AzureDemoWebApp/AzureDemoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureDemoWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDemoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
